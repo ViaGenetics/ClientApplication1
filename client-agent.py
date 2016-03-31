ba055f267fda011a1bd681333a38d336bbaa3b9e
RafaAcosta <RAcosta2@med.miami.edu>
2016-03-31 16:54:43 -0400

    Update client-agent.py

--- client-agent.py
+++ client-agent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 import subprocess, sys, logging, os
 from Genesis.GenesisClientCoordinator import GenesisClientCoordinator as GenesisClientCoordinator 
 
